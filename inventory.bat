8aa427b94d1bee4d0e9d37e7471c31d91bb4531d
Dustin Christlieb <34485743+dchristlieb@users.noreply.github.com>
2024-04-12 14:16:13 -0700

    Create inventory.bat

---
+++ inventory.bat
@@ -0,0 +1,16 @@
+gam print domains todrive
+gam print users allfields todrive
+gam print group settings todrive
+gam print group-members todrive
+gam print aliases todrive
+gam print teamdrives todrive
+gam all users print teamdriveacls oneitemperrow todrive
+gam print cros todrive
+gam print mobile todrive
+gam all users print forwardingaddresses todrive
+gam all users print delegates todrive
+gam all users print contactdelegates todrive
+gam print orgs name description parent inherit allfields todrive
+gam print resources allfields todrive
+gam print licenses todrive
+gam report domain todrive
